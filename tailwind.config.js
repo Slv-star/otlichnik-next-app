732f1384d0b30f02ccd1cadbca526231cccfb007
Slv-star <s_murzin@mail.ru>
2025-04-25 21:42:45 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#1E3A8A', // Добавить новый цвет
+      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      boxShadow: {
+        'inner-light': 'inset 0 1px 3px rgba(0, 0, 0, 0.12)',
+      },
+    },
   },
   plugins: [],
-};
+}
