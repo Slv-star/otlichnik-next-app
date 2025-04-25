4c5e68ef7c94bcda9b1d00de4c9692291d12fa2a
Slv-star <s_murzin@mail.ru>
2025-04-25 15:19:34 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
-<... truncated full code from canvas ...>
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">Привет!</h1>
+    </main>
+  );
+}
